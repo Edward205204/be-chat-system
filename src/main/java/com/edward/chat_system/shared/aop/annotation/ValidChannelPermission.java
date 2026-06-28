e398a3987505c5f2c8174f167199dcf55452a59c
Edward205204 <nguyentminhkhoa1@gmail.com>
2026-06-28 23:11:02 +0700

    feat(permission): add ValidChannelPermission annotation and validator

---
+++ src/main/java/com/edward/chat_system/shared/aop/annotation/ValidChannelPermission.java
@@ -0,0 +1,20 @@
+package com.edward.chat_system.shared.aop.annotation;
+
+import com.edward.chat_system.shared.aop.aspect.ValidServerPermissionValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ValidServerPermissionValidator.class)
+public @interface ValidChannelPermission {
+    String message() default "Invalid permission";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/edward/chat_system/shared/aop/aspect/ValidChannelPermissionValidator.java
@@ -0,0 +1,21 @@
+package com.edward.chat_system.shared.aop.aspect;
+
+import com.edward.chat_system.features.channel.enums.ChannelPermissionKeyEnum;
+import com.edward.chat_system.features.server.enums.ServerPermissionKeyEnum;
+import com.edward.chat_system.shared.aop.annotation.ValidChannelPermission;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class ValidChannelPermissionValidator
+        implements ConstraintValidator<ValidChannelPermission, String> {
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext ctx) {
+        if (value == null) return true; // @NotBlank handle null
+        try {
+            ChannelPermissionKeyEnum e = ChannelPermissionKeyEnum.valueOf(value);
+            return e != ChannelPermissionKeyEnum.NONE;
+        } catch (IllegalArgumentException ex) {
+            return false;
+        }
+    }
+}
