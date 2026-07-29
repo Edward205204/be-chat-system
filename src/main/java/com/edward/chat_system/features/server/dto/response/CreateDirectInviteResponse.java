0c5560bfe61b1991feafb3244836e72c50399ef6
Edward205204 <khoa123kd56cn@gmail.com>
2026-07-29 20:32:47 +0700

    docs: bruno api

---
+++ src/main/java/com/edward/chat_system/features/server/dto/response/CreateDirectInviteResponse.java
@@ -0,0 +1,28 @@
+package com.edward.chat_system.features.server.dto.response;
+
+import java.time.LocalDateTime;
+import com.edward.chat_system.features.server.enums.InviteStatusEnum;
+import com.edward.chat_system.features.user.dto.response.UserBasicInfoResponse;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CreateDirectInviteResponse {
+    String id;
+    ServerBasicInfoResponse server;
+    UserBasicInfoResponse inviter;
+    UserBasicInfoResponse invitee;
+    InviteStatusEnum status;
+    LocalDateTime expiresAt;
+    LocalDateTime createdAt;
+}
---
+++ src/main/java/com/edward/chat_system/features/server/dto/response/ServerBasicInfoResponse.java
@@ -0,0 +1,7 @@
+package com.edward.chat_system.features.server.dto.response;
+
+public class ServerBasicInfoResponse {
+    String id;
+    String name;
+    String avatar;
+}
